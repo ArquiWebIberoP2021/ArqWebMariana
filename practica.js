e8cee06092a56b8d88054e3f42223a36958b996a
aoimashup <38709218+aoimashup@users.noreply.github.com>
2021-05-06 09:58:11 -0500

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,25 @@
+class car {
+    constructor(state, color,type) {
+      this.state = state
+      this.color = color
+      this.type = type
+    }
+  }
+
+  class truck {
+    constructor(state,color, type) {
+      this.state = state
+      this.color = color
+      this.type = type
+    }
+  }
+
+
+
+let newCar = new car ('Terminado', 'rojo','Sedan');
+console.log(newCar);
+
+
+  
+
+  
